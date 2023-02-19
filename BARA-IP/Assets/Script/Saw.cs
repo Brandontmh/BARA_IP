de3c182ea8964d52273d29c812e49bae86bebfce
Brandontmh <s10222989@connect.np.edu.sg>
2023-02-19 23:51:43 +0800

    update

--- BARA-IP/Assets/Script/Saw.cs
+++ BARA-IP/Assets/Script/Saw.cs
@@ -4,17 +4,37 @@ using UnityEngine;
 
 public class Saw : MonoBehaviour
 {
-    bool cutting = false;
+    bool logExist = true;
+    public ParticleSystem dustParticle;
+    public GameObject chairLeg1;
+    public GameObject chairLeg2;
+    public GameObject chairLeg3;
+    public GameObject chairLeg4;
+
+
 
     private void Update()
     {
-       
+        if(logExist == false)
+        {
+            dustParticle.Play();
+        }
+
+        else
+        {
+            dustParticle.Stop();
+        }
+
     }
+
     public void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Log")
+        if (other.tag == "Log" && logExist == true)
         {
-            Debug.Log("Collided wiht " + other);
+            logExist = false;
+            Debug.Log("logExist is true");
+            Debug.Log("Collided with " + other);
+            
             StartCoroutine(waiter());
 
             IEnumerator waiter()
@@ -22,7 +42,14 @@ public class Saw : MonoBehaviour
                 yield return new WaitForSeconds(5);
                 Debug.Log("waiting");
                 Destroy(other.gameObject);
-                
+                logExist = true;
+                Debug.Log("logExist is false");
+                Instantiate(chairLeg1, transform.position, Quaternion.identity);
+                Instantiate(chairLeg2, transform.position, Quaternion.identity);
+                Instantiate(chairLeg3, transform.position, Quaternion.identity);
+                Instantiate(chairLeg4, transform.position, Quaternion.identity);
+
+
             }
         }
     }
