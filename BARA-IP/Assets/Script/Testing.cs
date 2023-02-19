657e8b53803c3afe1613593c88eb239e5d282cc1
Brandontmh <s10222989@connect.np.edu.sg>
2023-02-19 21:54:25 +0800

    Update

---
+++ BARA-IP/Assets/Script/Testing.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    bool cutting = false;
+
+    private void Update()
+    {
+       
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Log")
+        {
+            Debug.Log("Collided wiht " + other);
+            StartCoroutine(waiter());
+
+            IEnumerator waiter()
+            {
+                yield return new WaitForSeconds(5);
+                Debug.Log("waiting");
+                Destroy(other.gameObject);
+                
+            }
+        }
+    }
+    
+
+}
+    
