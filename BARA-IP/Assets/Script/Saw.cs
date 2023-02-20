cd7af6c4612044334b6bc89a83117e423a2aff6d
Brandontmh <s10222989@connect.np.edu.sg>
2023-02-20 17:53:11 +0800

    Chair Spawning Done

--- BARA-IP/Assets/Script/Saw.cs
+++ BARA-IP/Assets/Script/Saw.cs
@@ -6,10 +6,9 @@ public class Saw : MonoBehaviour
 {
     bool logExist = true;
     public ParticleSystem dustParticle;
-    public GameObject chairLeg1;
-    public GameObject chairLeg2;
-    public GameObject chairLeg3;
-    public GameObject chairLeg4;
+    public GameObject[] chair;
+
+
 
 
 
@@ -29,12 +28,12 @@ public class Saw : MonoBehaviour
 
     public void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Log" && logExist == true)
+        if (other.tag == "LogChairLeg" && logExist == true)
         {
             logExist = false;
             Debug.Log("logExist is true");
             Debug.Log("Collided with " + other);
-            
+
             StartCoroutine(waiter());
 
             IEnumerator waiter()
@@ -44,16 +43,48 @@ public class Saw : MonoBehaviour
                 Destroy(other.gameObject);
                 logExist = true;
                 Debug.Log("logExist is false");
-                Instantiate(chairLeg1, transform.position, Quaternion.identity);
-                Instantiate(chairLeg2, transform.position, Quaternion.identity);
-                Instantiate(chairLeg3, transform.position, Quaternion.identity);
-                Instantiate(chairLeg4, transform.position, Quaternion.identity);
+                chair[0].SetActive(true);
+                chair[1].SetActive(true);
+                chair[2].SetActive(true);
+                chair[3].SetActive(true);
+            }
+        }
+        else if (other.tag == "LogChairSeat" && logExist == true)
+        {
+            logExist = false;
+            Debug.Log("logExist is true");
+            Debug.Log("Collided with " + other);
 
+            StartCoroutine(waiter1());
 
+            IEnumerator waiter1()
+            {
+                yield return new WaitForSeconds(5);
+                Debug.Log("waiting");
+                Destroy(other.gameObject);
+                logExist = true;
+                Debug.Log("logExist is false");
+                chair[4].SetActive(true);
+            }
+        }
+        else if (other.tag == "LogChairRest" && logExist == true)
+        {
+            logExist = false;
+            Debug.Log("logExist is true");
+            Debug.Log("Collided with " + other);
+
+            StartCoroutine(waiter2());
+
+            IEnumerator waiter2()
+            {
+                yield return new WaitForSeconds(5);
+                Debug.Log("waiting");
+                Destroy(other.gameObject);
+                logExist = true;
+                Debug.Log("logExist is false");
+                chair[5].SetActive(true);
             }
         }
     }
-    
-
 }
     
